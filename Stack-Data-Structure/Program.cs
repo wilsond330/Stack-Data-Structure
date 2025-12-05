a70a87a19fec901b720805566e74bd9e0f67e84f
danny <danjameswilson101@gmail.com>
2025-12-05 14:37:49 +0000

    Begun testing stack, added Peek()

--- Stack-Data-Structure/Program.cs
+++ Stack-Data-Structure/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Stack<string> stack = new(5);
+            Console.WriteLine("Empty: " + stack.IsEmpty());
+            Console.WriteLine("Full: " + stack.IsFull());
+            stack.Push("Book 1");
+            stack.Push("Book 2");
+            Console.WriteLine(stack.Peek());
+            Console.WriteLine(stack.Pop());
+            stack.Push("Book 3");
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
         }
     }
 }
--- Stack-Data-Structure/Stack.cs
+++ Stack-Data-Structure/Stack.cs
@@ -36,14 +36,29 @@ public class Stack<stackType>
 		}
 		return (output, successful);
 	}
+	public (stackType, bool) Peek()
+	{
+        stackType output = default(stackType);
+        bool successful;
+        if (!isEmpty)
+        {
+            output = stack[top];
+            successful = true;
+        }
+        else
+        {
+            successful = false;
+        }
+        return (output, successful);
+    }
 	public bool Push(stackType input)
 	{
 		bool successful;
 		if (!isFull)
 		{
 			isEmpty = false;
-			stack[top] = input;
-			top++;
+            top++;
+            stack[top] = input;
 			if (top == MAX_LENGTH)
 			{
 				isFull = true;
